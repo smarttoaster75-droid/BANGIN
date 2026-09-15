a293b5633391351c7c1f10cb2fc6ea8c53b60e66
smarttoaster75-droid <smarttoaster75@gmail.com>
2026-09-15 02:51:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const dreh = document.getElementById("btn");
+const bo = document.querySelector("body");
+const t = document.querySelector("h3");
+function hszg(){
+bo.classList.add("class")
+t.textContent = "Programmieren zu können ist ganz nett. Der vollständige Code ist veröffentlicht unter: https://smarttoaster75-droid.github.io/BANGIN/."
+}
